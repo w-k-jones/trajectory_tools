b0f0019f76fe16d16a931290b1ddd5a085be7ed6
William Jones <william.jones@physics.ox.ac.uk>
2020-07-09 16:44:18 +0100

    Add setuptools and cleanup

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from trajectory_tools import *
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='trajectory tools',
+      version='0.1',
+      description='Python module to create and analyse HySPLIT trajectories',
+      url='',
+      author='William Jones',
+      author_email='william.jones@physics.ox.ac.uk',
+      license='GNU',
+      packages=['trajectory_tools'],
+      install_requires=[],
+      zip_safe=False)
